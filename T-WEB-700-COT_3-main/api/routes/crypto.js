6b646f4b447c3cb7d1566102fad2dfd38c7c2023
lbx18 <jojolucky221@gmail.com>
2024-10-11 08:47:33 +0000

    Delete T-WEB-700-COT_3-main directory

--- T-WEB-700-COT_3-main/api/routes/crypto.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const controller = require("../controllers/crypto_controller") 
-
-router.get("/" , controller.cryptos);
-
-module.exports = router;
