377697419dc5cff432780ebfdcacf79bc026ee50
lbx18 <jojolucky221@gmail.com>
2024-10-28 22:16:32 +0000

    Delete T-WEB-700-COT_3-main directory

--- T-WEB-700-COT_3-main/api/routes/connect.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const controller = require("../controllers/connect_controller")
-
-router.post("/register" , controller.register)
-
-router.post("/login" , controller.login)
-
-module.exports = router;
